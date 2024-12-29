371706447d0e0458ad3f8d60bdcdddee778220ce
Acid <masatozhongcun808@gmail.com>
2024-12-29 19:32:32 +0900

    effect changer complete but its buggy, fixing now
    Co-authored-by: tom

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,29 +1,57 @@
-//Unused but using soon?
-import React, { FC, MouseEventHandler, ReactNode } from "react";
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
+import { cva, type VariantProps } from "class-variance-authority"
 
-interface ButtonProps {
-  children: ReactNode;
-  onClick?: MouseEventHandler<HTMLButtonElement>;
-  className?: string;
+import { cn } from "@/lib/utils"
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+        destructive:
+          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+        outline:
+          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+        secondary:
+          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 hover:underline",
+      },
+      size: {
+        default: "h-9 px-4 py-2",
+        sm: "h-8 rounded-md px-3 text-xs",
+        lg: "h-10 rounded-md px-8",
+        icon: "h-9 w-9",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+)
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
 }
 
-const Button: FC<ButtonProps> = ({
-  children,
-  onClick,
-  className = "",
-}: {
-  children: ReactNode;
-  onClick?: MouseEventHandler<HTMLButtonElement>;
-  className?: string;
-}) => {
-  return (
-    <button
-      className={`relative inline-flex items-center justify-center whitespace-nowrap transition duration-200 ease-out outline-none focus:outline-none disabled:pointer-events-none bg-zinc-800 text-white shadow-lg hover:bg-[#34343a] h-11 gap-1 rounded-xl px-[18px] text-sm font-medium leading-5 tracking-[-.007em] [box-shadow:0_16px_8px_rgba(31,_31,_31,_0.01),_0_12px_6px_rgba(31,_31,_31,_0.04),_0_4px_4px_rgba(31,_31,_31,_0.07),_0_1.5px_3px_rgba(31,_31,_31,_0.08),_0_0_0_1px_#0f0f0f,_inset_0_1px_2px_hsla(0,_0%,_100%,_0.12)] ${className}`}
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
-};
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button"
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Button.displayName = "Button"
 
-export default Button;
+export { Button, buttonVariants }
